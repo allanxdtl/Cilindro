c562504971cd4ce2ed21fabbefd2ab56a53024d8
Bryan Valdez <alan272_@hotmail.com>
2022-05-14 13:04:54 -0700

    Nuevo proyecto de archivos de texto

---
+++ Archivos de Texto/Archivos de Texto/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Archivos_de_Texto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEscribir = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtGuardar = new System.Windows.Forms.Button();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEscribir
+            // 
+            this.btnEscribir.Location = new System.Drawing.Point(305, 195);
+            this.btnEscribir.Name = "btnEscribir";
+            this.btnEscribir.Size = new System.Drawing.Size(75, 31);
+            this.btnEscribir.TabIndex = 0;
+            this.btnEscribir.Text = "Escribir";
+            this.btnEscribir.UseVisualStyleBackColor = true;
+            this.btnEscribir.Click += new System.EventHandler(this.btnEscribir_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(305, 243);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(75, 29);
+            this.btnLeer.TabIndex = 1;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Direccion";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(70, 20);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(216, 20);
+            this.txtDireccion.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 56);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(274, 251);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // txtGuardar
+            // 
+            this.txtGuardar.Location = new System.Drawing.Point(305, 114);
+            this.txtGuardar.Name = "txtGuardar";
+            this.txtGuardar.Size = new System.Drawing.Size(75, 29);
+            this.txtGuardar.TabIndex = 6;
+            this.txtGuardar.Text = "Guardar";
+            this.txtGuardar.UseVisualStyleBackColor = true;
+            this.txtGuardar.Click += new System.EventHandler(this.txtGuardar_Click);
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(305, 66);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 31);
+            this.btnAbrir.TabIndex = 5;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Abrir una nueva ventana";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 372);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtGuardar);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnEscribir);
+            this.Name = "Form1";
+            this.Text = "Archivos de texto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEscribir;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button txtGuardar;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Archivos de Texto/Archivos de Texto/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Archivos_de_Texto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEscribir_Click(object sender, EventArgs e)
+        {
+            TextWriter write = new StreamWriter("test.txt");
+            write.WriteLine("Puto el que lo lea");
+            write.Close();
+            MessageBox.Show("Listo!");
+
+            StreamWriter add = File.AppendText("test.txt");
+            add.WriteLine("Nueva Linea add");
+            add.WriteLine("Este es mi primer archivo de texto");
+            add.Close();
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            TextReader read = new StreamReader("test.txt");
+            //tambien se puede utilizar el ReadLine(), pero solo imprime el primer renglon
+            MessageBox.Show(read.ReadToEnd(),"Contenido del archivo de texto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            read.Close();
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.Title = "Busca tu archivo";
+                openFileDialog1.ShowDialog();
+                if (File.Exists(openFileDialog1.FileName))
+                {
+                    string texto = openFileDialog1.FileName;
+                    TextReader read = new StreamReader(texto);
+                    richTextBox1.Text = read.ReadToEnd();
+                    read.Close();
+                    txtDireccion.Text = texto;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error al abrir", "Atencion", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (saveFileDialog1.ShowDialog()==DialogResult.OK)
+                {
+                    if (File.Exists(saveFileDialog1.FileName))
+                    {
+                        string txt = saveFileDialog1.FileName;
+                        StreamWriter textSave = File.CreateText(txt);
+                        textSave.Write(richTextBox1.Text);
+                        textSave.Flush();
+                        textSave.Close();
+                        txtDireccion.Text = txt;
+                    }
+                    else
+                    {
+                        string txt = saveFileDialog1.FileName;
+                        StreamWriter textSave = File.CreateText(txt);
+                        textSave.Write(richTextBox1.Text);
+                        textSave.Flush();
+                        textSave.Close();
+                        txtDireccion.Text = txt;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error al guardar");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Formulario2 ventana = new Formulario2();
+            ventana.Show();
+            
+        }
+    }
+}
